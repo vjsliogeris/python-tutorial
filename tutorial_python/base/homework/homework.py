5d4de3d8286b868360707c3d8cf1d08007f02469
vjsliogeris <vytenis.sliogeris@gmail.com>
2024-12-26 23:54:00 +0200

    On comments

--- tutorial_python/base/homework/homework.py
+++ tutorial_python/base/homework/homework.py
@@ -1,1 +1,4 @@
 # Go on then.
+# Btw text starting with a # denotes that it's a comment and pyhton just ignores it.
+# This is so that other humans can understand what's going on.
+# It's generally a good idea to have these.
